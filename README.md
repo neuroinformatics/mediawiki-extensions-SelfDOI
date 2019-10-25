b626259affd2d3376ccbf0fdcdb30773a361c131
Yoshihiro OKUMURA <yoshihiro.okumura@riken.jp>
2019-10-25 17:30:42 +0900

    add latest code.

---
+++ README.md
@@ -0,0 +1,56 @@
+# MediaWiki Extension SelfDOI
+
+This extension provides `<selfDOI />` to render self page [doi](https://www.doi.org/).
+
+## Install
+
+To install this extension, add the following to LocalSettings.php.
+
+```PHP
+wfLoadExtension("SelfDOI");
+$wgSelfDOIPrefix = "10.XXXXXX"; // required
+```
+
+### Configuration variables
+
+- `$wgSelfDOIPrefix`
+  - set the DOI prefix of this site. (required)
+    - e.g. "10.14931" - INCF Japan Node
+  - default: `""`
+- `$wgSelfDOISiteId`
+  - set the site id which used for first part of the DOI suffix.
+    - e.g. "bsd" - Brain Science Dictionary
+  - default: `""`
+- `$wgSelfDOISchema`
+  - display schema: "none", "doi" or "url".
+    - examples:
+      - "none" : `10.14931/bsd.354`
+      - "doi" : `doi:10.14931/bsd.354`
+      - "url" : `https://doi.org/10.14931/bsd.354`
+  - default: `"none"`
+
+## Usage
+
+#### typical case.
+
+```MediaWiki
+<selfdoi />
+```
+
+#### override display schema to `url`.
+
+```MediaWiki
+<selfdoi schema="url" />
+```
+
+## License
+
+This software is licensed under the [GNU General Public License 2.0 or later](COPYING).
+
+## Authors
+
+- [Yoshihiro Okumura](https://github.com/orrisroot)
+
+## Usage examples
+
+- https://bsd.neuroinf.jp/ : Brain Science Dictionary project in Japanese.
---
+++ includes/Hooks.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace MediaWiki\Extension\SelfDOI;
+
+class Hooks
+{
+    /**
+     * register tag renderer callbacks.
+     *
+     * @param \Parser $parser
+     */
+    public static function onParserFirstCallInit(\Parser $parser)
+    {
+        $parser->setHook('selfdoi', [self::class, 'renderSelfDOI']);
+    }
+
+    /**
+     * render <selfdoi> tag.
+     *
+     * @param string   $input
+     * @param array    $args
+     * @param \Parser  $parser
+     * @param \PPFrame $frame
+     *
+     * @return string
+     */
+    public static function renderSelfDOI($input, array $args, \Parser $parser, \PPFrame $frame)
+    {
+        static $isFirst = true;
+        global $wgSelfDOIPrefix, $wgSelfDOISiteId, $wgSelfDOISchema;
+        if ('' === $wgSelfDOIPrefix) {
+            return self::renderErrorResponse('selfdoi-error-prefix');
+        }
+        $page_id = $parser->getTitle()->getArticleId();
+        $doi = $wgSelfDOIPrefix.'/'.('' !== $wgSelfDOISiteId ? $wgSelfDOISiteId.'.' : '').$page_id;
+        $url = 'https://doi.org/'.$doi;
+        $schema = isset($args['schema']) ? $args['schema'] : $wgSelfDOISchema;
+        $label = '';
+        switch ($schema) {
+            case 'none':
+                $label = $doi;
+                break;
+            case 'url':
+                $label = $url;
+                break;
+            case 'doi':
+                $label = sprintf('doi:%s', $doi);
+                break;
+            default:
+                return self::renderErrorResponse('selfdoi-error-schema');
+        }
+        if ($isFirst) {
+            // append meta tags
+            $title = $parser->getTitle()->getText();
+            $meta = sprintf('<meta name="citation_title" input="%s" />', htmlspecialchars($title, ENT_QUOTES))."\n";
+            $parser->mOutput->addHeadItem($meta);
+            $meta = sprintf('<meta name="citation_doi" input="%s" />', $doi)."\n";
+            $parser->mOutput->addHeadItem($meta);
+            $isFirst = false;
+        }
+
+        return self::renderResponse('<a href="'.$url.'">'.$label.'</a>');
+    }
+
+    /**
+     * render error response.
+     *
+     * @param string $msgid
+     */
+    private static function renderErrorResponse($msgid)
+    {
+        return self::renderResponse('<strong class="error">'.wfMessage($msgid)->inContentLanguage()->escaped().'</strong>');
+    }
+
+    /**
+     * render response.
+     *
+     * @param string $message
+     *
+     * @return string
+     */
+    private static function renderResponse($message)
+    {
+        return '<!-- MediaWiki extension SelfDOI -->'.$message.'<!-- End of SelfDOI -->';
+    }
+}
